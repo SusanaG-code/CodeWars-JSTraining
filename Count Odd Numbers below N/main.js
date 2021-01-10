90b0155e835ad524d5886bc50620b77ebdc8e68d
Susana <su.gonzalez.vejo@gmail.com>
2021-01-10 19:17:37 +0100

    Add filter inventors exercise

---
+++ Count Odd Numbers below N/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+const oddCount = (n) => Math.floor(n / 2);
+
+//Math floor Round a number downward to its nearest integer.
