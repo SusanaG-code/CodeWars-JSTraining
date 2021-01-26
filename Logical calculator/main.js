322fdaa66d7c61916ae274ecb6a8cd925e508d15
Susana <su.gonzalez.vejo@gmail.com>
2021-01-26 09:10:41 +0100

    Convert to arrow function

--- Logical calculator/main.js
+++ Logical calculator/main.js
@@ -1,34 +1,11 @@
 "use strict";
-//AND
-function andCalc(array) {
-  //confirm all are the same
-  if (array.every((x) => x === true) || array.every((x) => x === false)) {
-    return array[0];
-  } else {
-    return false;
-  }
-}
 
-//OR
-function orCalc(array) {
-  //if prev and curr are the same, return first
-  //if prev and curr are not the same, return true
-  return array.reduce((x, y, z) => {
-    return x === y ? x : true;
-  });
-}
-
-//XOR
-function xorCalc(array) {
-  return array.reduce((x, y, z) => x !== y);
-}
+var ops = {
+  AND: (a, b) => a && b,
+  OR: (a, b) => a || b,
+  XOR: (a, b) => a !== b,
+};
 
 function logicalCalc(array, op) {
-  if (op === "AND") {
-    return andCalc(array);
-  } else if (op === "OR") {
-    return orCalc(array);
-  } else if (op === "XOR") {
-    return xorCalc(array);
-  }
+  return array.reduce(ops[op]);
 }
