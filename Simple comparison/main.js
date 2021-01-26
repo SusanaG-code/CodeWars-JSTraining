a1ae23afd7b7826af3d3cc4c1b84a3c4956ea0b3
Susana <su.gonzalez.vejo@gmail.com>
2021-01-26 18:02:07 +0100

    Add simple comparison exercise

---
+++ Simple comparison/main.js
@@ -0,0 +1,14 @@
+"use strict";
+function add(a, b) {
+  if (a == b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+add(2, "2");
+
+//arrow function
+
+// const add = (a, b) => a == b
---
+++ Ternary operator-Sales hot-dogs/main.js
@@ -0,0 +1,2 @@
+"use strict";
+const saleHotdogs = (n) => n * (n < 5 ? 100 : n >= 5 && n < 10 ? 95 : 90);
